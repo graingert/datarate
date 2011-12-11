c70985878655ce2f0645fed782eca5f9eaca521b
Thomas Grainger <tagrain@gmail.com>
2011-12-11 23:59:41 +0000

    added django extensions

--- settings.py
+++ settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'reviews',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
