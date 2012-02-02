9418e39c0111d2a335cf01b1469b30c95dfd36d1
Thomas Grainger <tagrain@gmail.com>
2012-02-02 19:25:59 +0000

    added class based view for reviewable URI

--- reviews/views.py
+++ reviews/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+from reviews.models import *
+import urllib
+
+class ReviewableDetailView(DetailView):
+	queryset = Reviewable_URI.objects.all()
+
+	def get_object(self):
+		encoded_uri = self.kwargs["uri"]
+		uri = urllib.unquote(encoded_uri)
+		return Reviewable_URI(reviewed_uri = uri)
+		
+
