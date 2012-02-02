532b22bb8916617e8bea23d25ed22c43fde899fe
Thomas Grainger <tagrain@gmail.com>
2012-02-02 19:27:18 +0000

    added class based URI view

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from reviews import models as m
 from django.views import generic as g
+from reviews import views as v
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -21,5 +23,6 @@ urlpatterns += patterns('',
 	url(r'^$', g.TemplateView.as_view(template_name="index.html")),
 	url(r'^review/(?P<pk>\d+)$', g.DetailView.as_view(model=m.Review), name="review_detail"),
 	url(r'^review/$', g.ListView.as_view(model=m.Review)),
-	url(r'create-review/$',g.CreateView.as_view(model=m.Review)),
+	url(r'create-review/$',g.CreateView.as_view(model=m.Review))
+	url(r'^object/(?P<url>\d+)$', v.ReviewableDetailView.as_view(), name="reviewable_detail"),
 )
