66629422f019eedac80fa47bd509826429779607
Thomas Grainger <tagrain@gmail.com>
2012-02-13 13:41:01 +0000

    set firstname to first part of email address

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models as m
-from django.contrib.auth import models as authm
+from django.contrib.auth.models import User
 from django_extensions.db.fields import AutoSlugField, CreationDateTimeField, ModificationDateTimeField
 # Create your models here.
 
@@ -19,7 +19,7 @@ class Review(m.Model):
 	title = m.CharField(max_length=128)
 	text = m.TextField()
 	rating = m.IntegerField()
-	author = m.ForeignKey(authm.User)
+	author = m.ForeignKey(User)
 	reviewed_uri = m.ForeignKey(Thing)
 	
 	@m.permalink
@@ -28,3 +28,29 @@ class Review(m.Model):
 
 class ExtendedReview(Review):
 	extra = m.TextField()
+
+
+#Set the first_name to the first part of the email address
+from django.db.models.signals import post_save, pre_save
+
+def user_first_name(sender, instance, created, **kwargs):
+	if created:
+		instance.firstname = instance.email.rsplit("@",1)[0][:30]
+		instance.save()
+
+post_save.connect(user_first_name, sender=User)
+
+##cache gravatar md5
+#import urllib, hashlib
+#class UserProfile(m.Model):
+	## This field is required.
+	#user = m.OneToOneField(User)
+	#gravatar_hash = m.CharField(max_length=32)
+
+#def create_user_profile(sender, instance, created, **kwargs):
+	#if created:
+		#gravatar_hash = hashlib.md5(instance.email.lower()).hexdigest()
+		#UserProfile.objects.create(user=instance)
+
+
+#post_save.connect(create_user_profile, sender=User)
