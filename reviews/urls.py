2f1b3a1ba653cf951dd098e346b7f9c816e90954
Thomas Grainger <tagrain@gmail.com>
2012-02-14 19:55:57 +0000

    fixed potential bug with urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
 	url(r'^$', g.TemplateView.as_view(template_name="index.html")),
 	url(r'^review/(?P<pk>\d+)$', g.DetailView.as_view(model=m.Review), name="review_detail"),
 	url(r'^review/$', g.ListView.as_view(model=m.Review)),
-	url(r'create-review/$',g.CreateView.as_view(model=m.Review)),
+	url(r'^create-review/$',g.CreateView.as_view(model=m.Review)),
 	url(r'^things/(?P<slug>[-\w]+)$', v.ThingDetailView.as_view(model=m.Thing), name="thing_detail"),
 )
