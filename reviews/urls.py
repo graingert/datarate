9d6d3975d7ef46bb534e924b488d9f14508111a4
Thomas Grainger <tagrain@gmail.com>
2012-02-23 18:20:54 +0000

    used url template tag for nav

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,8 +7,8 @@ from reviews.views import ReviewCreateView, ThingDetailView
 urlpatterns = patterns('',
 	url(r'^$', g.TemplateView.as_view(template_name="index.html")),
 	url(r'^review/(?P<pk>\d+)$', g.DetailView.as_view(model=m.Review), name="review_detail"),
-	url(r'^review/$', g.ListView.as_view(model=m.Review)),
-	url(r'^thing/$', g.ListView.as_view(model=m.Thing)),
+	url(r'^review/$', g.ListView.as_view(model=m.Review), name="review"),
+	url(r'^thing/$', g.ListView.as_view(model=m.Thing), name="thing"),
 	url(r'^thing/(?P<slug>[-\w]+)/create-review$',ReviewCreateView.as_view(), name="create-review"),
 	url(r'^thing/(?P<slug>[-\w]+)$', ThingDetailView.as_view(), name="thing_detail"),
 )
