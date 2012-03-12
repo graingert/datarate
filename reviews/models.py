dac44330d543879260e30cc7c5862a339e107263
Thomas Grainger <tagrain@gmail.com>
2012-03-12 12:24:44 +0000

    Added validation to range field

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,32 @@
 from django.db import models as m
 from django.contrib.auth.models import User
 from django_extensions.db.fields import AutoSlugField, CreationDateTimeField, ModificationDateTimeField
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 import urllib, hashlib
 from rdflib import ConjunctiveGraph, Namespace, exceptions
 from rdflib import URIRef, RDFS, RDF, BNode
 import bleach
 # Create your models here.
+
+class StepValidator():
+	def __init__(min_value, step):
+		self.min_value = min_value
+		self.step = step
+		
+	def __call__(self, value):
+		if (value - min_value)% step != 0:
+			raise ValidationError(u'%s does not have the correct step' % value)
+		
+
+class RangeField(m.IntegerField):
+	def __init__(self, min_value, max_value, step=None, *args, **kwargs):
+		validators = kwargs.setdefault("validators",[])
+		validators += [MinValueValidator(min_value), MaxValueValidator(max_value)]
+		if step:
+			validators.append(StepValidator(min_value, step))
+		super(RangeField, self).__init__(*args, **kwargs)
+		
 
 class Thing(m.Model):
 	uri = m.URLField(verify_exists=False, unique=True)
@@ -44,7 +65,7 @@ class Review(m.Model):
 	date_modified = ModificationDateTimeField()
 	#title = m.CharField(max_length=128)
 	text = m.TextField()
-	rating = m.IntegerField()
+	rating = RangeField(min_value=-3, max_value = 3, step = 2)
 	author = m.ForeignKey(User, editable="false")
 	reviewed_uri = m.ForeignKey(Thing)
 	
