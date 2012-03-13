1235b58e45e9dcfc20dfa3ac749924e05c740840
Thomas Grainger <tagrain@gmail.com>
2012-03-13 15:33:38 +0000

    Fixed ordering on Recent Reviews

--- reviews/views.py
+++ reviews/views.py
@@ -22,7 +22,7 @@ class PreviewView(TemplateView):
 		
 		context["best"] = total_scores.order_by("-review__rating__sum")[:5]
 		context["worst"] = total_scores.order_by("review__rating__sum")[:5]
-		context["reviews"] = Review.objects.all().order_by("date_created")[:5]
+		context["reviews"] = Review.objects.all().order_by("-date_created")[:5]
 				
 		return context
 
