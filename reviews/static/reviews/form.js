c100f16fa3c63518ff67d3db7dc7848665556462
Thomas Grainger <tagrain@gmail.com>
2012-06-25 15:15:04 +0100

    fix javascript uri XSS attack in review mentions

--- reviews/static/reviews/form.js
+++ reviews/static/reviews/form.js
@@ -199,7 +199,10 @@ $(function(){
 					for(var i=0; i<json.results.bindings.length; i++){
 						var item = json.results.bindings[i]
 						if (item.thing.type == "uri" && 'name' in item){
-							mentionables.add(new Mentionable({ uri:item.thing.value, label:item.name.value}))
+							var prot = URI(item.s.value).protocol()
+							if (prot == "http" || prot == "https" ){
+								mentionables.add(new Mentionable({ uri:item.thing.value, label:item.name.value}))
+							}
 						}
 					}
 					done()
